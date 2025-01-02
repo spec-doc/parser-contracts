2470f2c718538ac47d3f0e2a018f50806f33df8e
Ivan Mezinov <ivanmezinov@mail.ru>
2025-01-03 01:43:15 +0300

    add main methods

--- Parser/ParserInterface.php
+++ Parser/ParserInterface.php
@@ -19,4 +19,22 @@ interface ParserInterface
      * @throws ParserExceptionInterface Any parser error
      */
     public function parse(): iterable;
+
+    /**
+     * Sets the content for parsing.
+     *
+     * @param string $content
+     *
+     * @return self
+     */
+    public function setContent(string $content): self;
+
+    /**
+     * Sets the rules for content parsing.
+     *
+     * @param iterable $rules
+     *
+     * @return self
+     */
+    public function setRules(iterable $rules): self;
 }
