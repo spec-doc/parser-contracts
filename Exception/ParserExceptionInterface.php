ba84b4e9e5f11e90c658b8ceed559e4bc2d6bb5d
Ivan Mezinov <ivanmezinov@mail.ru>
2025-01-02 16:13:42 +0300

    move ParserExceptionInterface

---
+++ Exception/ParserExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpecDoc\Contract\Exception;
+
+/**
+ * This exception interface describing any parser errors.
+ */
+interface ParserExceptionInterface extends \Throwable {}
