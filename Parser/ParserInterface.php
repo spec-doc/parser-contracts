8703ce1af1188de2a7ad1244299a31efa9f7cad0
Ivan Mezinov <ivanmezinov@mail.ru>
2025-02-19 20:34:26 +0300

    add Rule contracts in dev-section

--- Parser/ParserInterface.php
+++ Parser/ParserInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SpecDoc\Contract\Parser;
 
+use SpecDoc\Contract\Rule\ParseRuleInterface;
 use SpecDoc\Contract\Token\TokenInterface;
 use SpecDoc\Contract\Exception\ParserExceptionInterface;
 
@@ -16,9 +17,10 @@ interface ParserInterface
      * Parses the source into a list of tokens.
      *
      * @param string $content
+     * @param array<ParseRuleInterface> $rules
      *
      * @return list<TokenInterface>
      * @throws ParserExceptionInterface Any parser error
      */
-    public function parse(string $content): iterable;
+    public function parse(string $content, array $rules): iterable;
 }
